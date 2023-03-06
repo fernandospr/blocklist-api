43c3b602bdebf278af7cab95ed4639aa0880e4ac
Fernando Sproviero <fernandospr@gmail.com>
2023-03-06 20:53:40 -0300

    Caching the blocklist

--- src/main/kotlin/com/github/fernandospr/blocklist/BlocklistApplication.kt
+++ src/main/kotlin/com/github/fernandospr/blocklist/BlocklistApplication.kt
@@ -2,7 +2,9 @@ package com.github.fernandospr.blocklist
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
+@EnableCaching
 @SpringBootApplication
 class BlocklistApplication
 
--- src/main/kotlin/com/github/fernandospr/blocklist/BlocklistService.kt
+++ src/main/kotlin/com/github/fernandospr/blocklist/BlocklistService.kt
@@ -1,6 +1,7 @@
 package com.github.fernandospr.blocklist
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,6 +10,7 @@ class BlocklistService(
   @Autowired private val ipExtractor: Ipv4Extractor
 ) {
 
+  @Cacheable(value = ["blacklist"])
   fun getIpBlocklist(): Collection<String> {
     val blocklistStr = client.getIpBlocklist()
     return ipExtractor.fromString(blocklistStr)
