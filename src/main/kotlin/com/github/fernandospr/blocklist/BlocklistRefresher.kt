1b0c2d21b11b91c6850b60517f1dff60ef669948
Fernando Sproviero <fernandospr@gmail.com>
2023-03-06 21:12:57 -0300

    Scheduling to refresh the blocklist updating the cache at 00:00 everyday

---
+++ src/main/kotlin/com/github/fernandospr/blocklist/BlocklistRefresher.kt
@@ -0,0 +1,39 @@
+package com.github.fernandospr.blocklist
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+/**
+ * Refresh at 00:00:00 everyday by default.
+ */
+const val BLOCKLIST_REFRESH_CRON_DEFAULT = "0 0 0 * * *"
+
+@Component
+class BlocklistRefresher(
+  @Autowired private val service: BlocklistService,
+  private val logger: Logger = LoggerFactory.getLogger(BlocklistRefresher::class.java)
+) {
+
+  @EventListener(ApplicationReadyEvent::class)
+  fun refreshOnApplicationReady() {
+    refreshBlocklistCache()
+  }
+
+  @Scheduled(cron = BLOCKLIST_REFRESH_CRON_DEFAULT)
+  fun scheduleRefresh() = refreshBlocklistCache()
+
+  private fun refreshBlocklistCache() {
+    try {
+      logger.info("Refreshing the blocklist...")
+      val blocklist = service.getIpBlocklistAndUpdateCache()
+      logger.info("Blocklist refreshed with ${blocklist.size} ips.")
+    } catch (e: Exception) {
+      logger.error("Couldn't refresh the blocklist.")
+    }
+  }
+}
--- src/main/kotlin/com/github/fernandospr/blocklist/BlocklistService.kt
+++ src/main/kotlin/com/github/fernandospr/blocklist/BlocklistService.kt
@@ -1,6 +1,7 @@
 package com.github.fernandospr.blocklist
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.cache.annotation.CachePut
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
@@ -10,8 +11,14 @@ class BlocklistService(
   @Autowired private val ipExtractor: Ipv4Extractor
 ) {
 
-  @Cacheable(value = ["blacklist"])
+  @Cacheable(value = ["blocklist"])
   fun getIpBlocklist(): Collection<String> {
+    val blocklistStr = client.getIpBlocklist()
+    return ipExtractor.fromString(blocklistStr)
+  }
+
+  @CachePut(value = ["blocklist"])
+  fun getIpBlocklistAndUpdateCache(): Collection<String> {
     val blocklistStr = client.getIpBlocklist()
     return ipExtractor.fromString(blocklistStr)
   }
