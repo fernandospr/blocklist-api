d67a142d2535af9f423ef3a77445786fd4859cb7
Fernando Sproviero <fernandospr@gmail.com>
2023-03-06 23:05:56 -0300

    Retrying to obtain the blocklist if server or network is down

--- src/main/kotlin/com/github/fernandospr/blocklist/BlocklistApplication.kt
+++ src/main/kotlin/com/github/fernandospr/blocklist/BlocklistApplication.kt
@@ -3,10 +3,12 @@ package com.github.fernandospr.blocklist
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.retry.annotation.EnableRetry
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableCaching
 @EnableScheduling
+@EnableRetry
 @SpringBootApplication
 class BlocklistApplication
 
--- src/main/kotlin/com/github/fernandospr/blocklist/IPSumClient.kt
+++ src/main/kotlin/com/github/fernandospr/blocklist/IPSumClient.kt
@@ -1,14 +1,38 @@
 package com.github.fernandospr.blocklist
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
+
+/**
+ * Retry 5 times by default.
+ */
+const val BLOCKLIST_REST_CLIENT_RETRY_MAXATTEMPTS_DEFAULT = 5
+
+/**
+ * Use a backoff delay of 5000ms by default.
+ */
+const val BLOCKLIST_REST_CLIENT_RETRY_DELAY_DEFAULT = 5000
+
+/**
+ * Use a backoff multiplier of 2.0 by default.
+ */
+const val BLOCKLIST_REST_CLIENT_RETRY_MULTIPLIER_DEFAULT = 2.0
 
 @Component
 class IPSumClient(
   @Autowired private val restTemplate: RestTemplate
 ) {
 
+  @Retryable(
+    maxAttemptsExpression = "#{\${blocklist.rest.client.retry.maxAttempts:$BLOCKLIST_REST_CLIENT_RETRY_MAXATTEMPTS_DEFAULT}}",
+    backoff = Backoff(
+      delayExpression = "#{\${blocklist.rest.client.retry.backoff.delay:$BLOCKLIST_REST_CLIENT_RETRY_DELAY_DEFAULT}}",
+      multiplierExpression = "#{\${blocklist.rest.client.retry.backoff.multiplier:$BLOCKLIST_REST_CLIENT_RETRY_MULTIPLIER_DEFAULT}}"
+    )
+  )
   fun getIpBlocklist(): String {
     val result = restTemplate.getForEntity(url, String::class.java)
     return result.body.orEmpty()
